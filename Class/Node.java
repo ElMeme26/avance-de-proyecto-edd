14a968a900100cfb8335e34e43cb33b406734e1e
Leonardo Pereda <leonardopereda7@hotmail.com>
2025-08-22 19:00:36 -0600

    Node completo

--- Class/Node.java
+++ Class/Node.java
@@ -1,4 +1,12 @@
 package Class;
+
 public class Node {
     
+    String data;
+    Node forward;
+
+    public Node(String data) {
+        this.data = data;
+        this.forward = null;
+    }
 }
