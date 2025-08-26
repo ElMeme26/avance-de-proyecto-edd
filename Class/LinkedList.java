f63829de5065688a20c0bf4be9a0b55557c27c4b
Leonardo Pereda <leonardopereda7@hotmail.com>
2025-08-25 18:59:50 -0600

    LinkedList completo

--- Class/LinkedList.java
+++ Class/LinkedList.java
@@ -71,7 +71,43 @@ public class LinkedList {
         }
     }
 
-    public void find() {
+    public void findByDepartment(String department) {
+        if (isEmpty()) {
+            System.out.println("No hay tareas pendientes");
+            return;
+        }
+
+        Node iteratorNode = head;
+        boolean found = false;
+
+        while (iteratorNode != null) {
+            if (iteratorNode.data.startsWith(department + ":")) {
+                System.out.println(iteratorNode.data);
+                found = true;
+            }
+
+            iteratorNode = iteratorNode.forward;
+        }
         
+        if (!found) {
+            System.out.println("No se encontraron tareas para el departamento: " + department);
+        }
+    }
+
+    public void show() {
+        if (isEmpty()) {
+            System.out.println();
+        }
+
+        Node iteratorNode = head;
+
+        while (iteratorNode != null) {
+            System.out.print(iteratorNode.data);
+            if (iteratorNode.forward != null) {
+                System.out.println(" -> ");
+            }
+            
+            iteratorNode = iteratorNode.forward;
+        }
     }
 }
